29671bd20ee79794b7602490be16fbb2f1a5af39
Pedro Batista <pmpb742k2@gmail.com>
2022-07-29 23:32:11 +0100

    Added DB backup file and route to get users associated with a certain hive

---
+++ src/Controllers/GetHiveUsers.js
@@ -0,0 +1,31 @@
+const handleGetHiveUsers = (db) => (req, res) => {
+  const { hive_id } = req.body;
+  const { user_id } = req.user;
+
+  return db("user_hives")
+    .select("hive_id")
+    .where({
+      hive_id,
+      hive_user_id: user_id,
+    })
+    .then((data) => {
+      console.log(data);
+      if (data[0].hive_id !== hive_id)
+        return res.status(301).json({
+          error: "You do not have permission to access this hive info",
+        });
+
+      return db("user_hives")
+        .join("login", "user_hives.hive_user_id", "login.user_id")
+        .join("users", "login.username", "users.username")
+        .select("login.email", "users.name")
+        .where({ hive_id })
+        .then((data) => res.json(data))
+        .catch((error) =>
+          res.status(500).json({ error: "Internal Server Error" })
+        );
+    })
+    .catch((error) => res.json({ error: "Internal Server Error" }));
+};
+
+module.exports = { handleGetHiveUsers };
--- src/routes.js
+++ src/routes.js
@@ -12,6 +12,7 @@ const { handleRemoveHive } = require("./Controllers/RemoveHive");
 const { handleGetUserProfile } = require("./Controllers/GetUserProfile");
 const { handleUpdateUserProfile } = require("./Controllers/UpdateUserProfile");
 const { handleUpdatePassword } = require("./Controllers/UpdatePassword");
+const { handleGetHiveUsers } = require("./Controllers/GetHiveUsers");
 
 const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
 
@@ -27,24 +28,37 @@ const db = knex({
 
 const router = express.Router();
 
+// Receive data from the eletronic device
 router.post("/receive-data", handleReceiveData(db));
 
+// Register and authenticate a new user
 router.post("/register", handleRegister(db, bcrypt));
 
+// Authenticate a user
 router.post("/login", handleLogin(db, bcrypt));
 
+// Add a new hive or update its details
 router.post("/add-hive", auth, handleAddHive(db));
 
+// Get the hives of an user
 router.get("/get-hives", auth, handleGetHives(db));
 
+// Get the readings of an hive
 router.post("/get-hive-data", auth, handleGetHiveData(db));
 
+// Remove an hive from a user
 router.delete("/remove-hive", auth, handleRemoveHive(db));
 
+// Get a user profile
 router.get("/get-user-profile", auth, handleGetUserProfile(db));
 
+// Update a user profile
 router.put("/update-user-profile", auth, handleUpdateUserProfile(db));
 
+// Update a user password
 router.put("/update-password", auth, handleUpdatePassword(db, bcrypt));
+
+// Get the users associated with a hive
+router.post("/get-hive-users", auth, handleGetHiveUsers(db));
 
 module.exports = router;
