2ac8f642686f2608d2594b2f627b04b0557fca1a
Pedro Batista <pmpb742k2@gmail.com>
2022-08-20 11:50:43 +0100

    Added morgan logger

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,16 @@ import cors from "cors";
 import helmet from "helmet";
 require("dotenv").config();
 import { router } from "./routes";
+const morgan = require("morgan");
 
 export const app = express();
 
-app.use(cors());
+app.use(morgan("combined"));
+app.use(
+  cors({
+    origin: ["https://pedro742k2.github.io", "http://localhost:5173"],
+  })
+);
 app.use(express.json());
 app.use(helmet());
 app.use("/api/", router);
